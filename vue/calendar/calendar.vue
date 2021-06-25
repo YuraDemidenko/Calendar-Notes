aaf2779a41fd893f6a85380a624e8a74b0129920
YuraDemidenko <83308882+YuraDemidenko@users.noreply.github.com>
2021-06-25 22:06:15 +0300

    Create calendar.vue

---
+++ vue/calendar/calendar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="global-container">
+
+        <calendar></calendar>
+    
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: function () {
+            return{
+                
+            }
+        },
+
+        components: {
+            'calendar': require('./calendar/calendar.vue'),
+        },
+
+        
+
+    }
+</script>
